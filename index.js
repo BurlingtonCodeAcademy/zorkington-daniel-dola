16c58bec9339d05a45996c33841fccc564c90b23
Anasser115 <69481338+Anasser115@users.noreply.github.com>
2021-02-05 16:39:09 -0500

    sanitation fixed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Rooms {
     //Room One
     roomDescription = "in front of cave",
     item = "staff",
-    nextRoom = "narrow passage",
+    nextRoom = this.narrowPassage,
     name = "entrance",
     door = false,
     secretDoor = false,
@@ -41,7 +41,7 @@ class Rooms {
   // Method to convert the current room to the 2nd room 'Narrow Passage'
   narrowPassage() {
     this.name = `Narrow Passage`;
-    this.nextRoom = `Treasure Room`;
+    this.nextRoom = this.treasureRoom;
     this.roomDescription = `Dark Room`;
     this.door = false;
     this.doorLock = `crawl`;
@@ -49,7 +49,7 @@ class Rooms {
   // Method to convert the current room to the 3rd room 'Treasure Room';
   treasureRoom() {
     this.name = `Treasure Room`;
-    this.nextRoom = `Graveyard Room`;
+    this.nextRoom = this.graveyardRoom;
     this.roomDescription = `Treasure room where the key is`;
     this.door = true;
     this.item = `key`;
@@ -58,7 +58,7 @@ class Rooms {
   graveyardRoom() {
     this.item = `Sword of Truth`;
     this.name = `Graveyard`;
-    this.nextRoom = `Throne Room`;
+    this.nextRoom = this.throneRoom;
     this.roomDescription = `Room filled with bones and a magical sword`;
     this.door = true;
     this.secretDoor = false;
@@ -69,14 +69,14 @@ class Rooms {
   ancientArmory() {
     this.item = `Shield of Light`;
     this.name = `Ancient Armory`;
-    this.nextRoom = `Graveyard`;
+    this.nextRoom = this.graveyardRoom;
     this.roomDescription = `Room filled mostly with old useless weapons`;
     this.door = true;
   }
   // Method to convert the current room to the 6th room `Throne Room1;
   throneRoom() {
     this.name = `Throne Room`;
-    this.nextRoom = `Graveyard Room`;
+    this.nextRoom = this.firstRoom;
     this.roomDescription = `Elegant Throne room inhabited by an evil dragon!`;
     this.door = true;
   }
@@ -84,7 +84,7 @@ class Rooms {
 
 //Player Template
 class Player {
-  constructor(playerName, playerInventory = "torch", status = "normal") {
+  constructor(playerName, playerInventory = ["torch"], status = "normal") {
     this.playerName = playerName;
     this.currentRoom;
     this.playerInventory = playerInventory;
@@ -133,12 +133,15 @@ class Player {
 //Main Game Sequence
 /////////////////////
 async function start() {
+  let player = new Player();
+  let currentRoom = new Rooms();
   //Establish Character Name
-  playerName = await ask(`What is your character's name?`);
+  playerName = await ask(`What is your character's name?\n>_`);
 
   //Player Instructions
-  await ask(`In order for ${playerName} to progress through the game you must type in the actions
-  in the format of...\n  verb  +   object's Name   example: open door\nThese will be key phrases hinted at throughout the game play, good luck!\n\nPress enter when you're ready...`);
+  await ask(
+    `In order for ${playerName} to progress through the game you must type in the actions in the format of...\n  verb  +   object's Name   example: open door\nThese will be key phrases hinted at throughout the game play, good luck!\n\nPress enter when you're ready...`
+  );
 
   //First Description
   const welcomeMessage = `After a long journey ${playerName} finally arrives at the dungeon. With the fatigue of many miles and the wizard who summoned you, you stand in front of ominous door that leads you to the great beast that has been plaguing the countryside.\nThe Wizard: "${playerName}! You are the chosen one, the ONLY one who can wield the power great enough to slay the dragon! Hold my staff to reveal the password sigils, speak the words and enter the cavern to fulfill your destiny!"\n >_`;
@@ -153,29 +156,28 @@ async function start() {
 
   // verb = string1[0].toLowerCase();
   // object = string1[1].toLowerCase();
-  console.log(object);
-  console.log(verb);
-  console.log(string1);
+  // console.log(object);
+  // console.log(verb);
+  // console.log(string1);
 
   // while loop with everything inside and if for each condition
-while(string1.length !== 2 || verb !== "hold" || object !== "staff"){
-
-  //loop if it is no 2 word
-  while (string1.length !== 2) {
-    console.log(`enter right format`);
-    answer = await ask(`>_`);
-    string1 = answer.split(" ");
-  }
-  console.log(verb);
-  console.log(object);
-  if (string1.length == 2) {
-    verb = string1[0].toLowerCase();
-    object = string1[1].toLowerCase();
-  }
-  // loop if it is not the desired answer
-  while (verb !== "hold" || object !== "staff") {
-    answer = await ask(
-      `You can't ${verb} the ${object}!\nWizard:${playerName}! you must hold the staff with me to reveal the sigils!!\n>_`
+  while (string1.length !== 2 || verb !== "hold" || object !== "staff") {
+    //loop if it is no 2 word
+    while (string1.length !== 2) {
+      console.log(`enter right format\n`);
+      answer = await ask(`>_`);
+      string1 = answer.split(" ");
+    }
+    //console.log(verb);
+    //console.log(object);
+    if (string1.length == 2) {
+      verb = string1[0].toLowerCase();
+      object = string1[1].toLowerCase();
+    }
+    // loop if it is not the desired answer
+    while (verb !== "hold" || object !== "staff") {
+      answer = await ask(
+        `You can't ${verb} the ${object}!\nWizard: ${playerName}! you must hold the staff with me to reveal the sigils!!\n>_`
       );
       string1 = answer.split(" ");
       if (string1.length == 2) {
@@ -186,19 +188,60 @@ while(string1.length !== 2 || verb !== "hold" || object !== "staff"){
   }
   // loop for the 1st task shows the spell and if answer is read spell goes on, if not gives a hint
   //Check with Dola, this might be 'and' instead of 'or'
-  if (verb == "hold" || object == "staff") {
-    answer = await ask(`The sigils appear around the stone door and grow brighter the longer you hold the staff...\nWizard: The spell has appeared! Now ${playerName}! Cast the spell!\n>_ `)
+  if (verb == "hold" && object == "staff") {
+    answer = await ask(
+      `The sigils appear around the stone door and grow brighter the longer you hold the staff...\nWizard: The spell has appeared! Now ${playerName}! Cast the spell!\n>_ `
+    );
+  }
+  /////////////////////////
+  ///sanitazation
+  string1 = answer.split(" ");
+  while (string1.length !== 2 || verb !== "cast" || object !== "spell") {
+    //loop if it is no 2 word
+    while (string1.length !== 2) {
+      console.log(`enter right format\n`);
+      answer = await ask(`>_`);
+      string1 = answer.split(" ");
+    }
+    if (string1.length == 2) {
+      verb = string1[0].toLowerCase();
+      object = string1[1].toLowerCase();
+      console.log(`New object1 ` +object);
+      console.log(`New verb1 `+verb);
     }
   
-  if (verb == "cast" && object == "spell") {
-    console.log(`The door slides open with in a slow rumble and finishes the process with a resounding BOOM!\n Wizard: The fate of the kingdom rests in your hands now ${playerName}!\nYou step inside the cave only to be engulfed by darkness...\n\n`)
-    answer = await ask(`${playerName} cannot see at all! ${playerName} looks for their torch to light...\n>_`)
+    // loop if it is not the desired answer
+    while (verb !== "cast" || object !== "spell") {
+      answer = await ask(
+        `You can't ${verb} the ${object}!\nWizard: ${playerName}! We should cast the spell togther!!\n>_`
+      );
+      string1 = answer.split(" ");
+      if (string1.length == 2) {
+        verb = string1[0].toLowerCase();
+        object = string1[1].toLowerCase();
+      }
+    }
   }
-  
+  ////////////////////////
+  if (verb == "cast" && object == "spell") {
+    console.log(
+      `The door slides open with in a slow rumble and finishes the process with a resounding BOOM!\n Wizard: The fate of the kingdom rests in your hands now ${playerName}!\nYou step inside the cave only to be engulfed by darkness...\n\n`
+    );
+    currentRoom.nextRoom();
+    console.log(currentRoom)
+    answer = await ask(
+      `It's dark in here. ${playerName} cannot see at all! ${playerName} looks for their torch to light...\n>_`
+    );
+  }
+
   //Narraw passage method
-  if(verb == "light" && object == "torch" ){
-    console.log(`The room blazes with illumination as the torch catches fire!\n`)
-    answer = await ask(`${playerName} looks around only and to see jagged walls in close proximity. They stretch downward making the small passage even more narrow with every inch. The only possible way to fit is to crawl on the floor...\n>_`)
+  if (verb == "light" && object == "torch") {
+    console.log(
+      `The room blazes with illumination as the torch catches fire!\n`
+    );
+    answer = await ask(
+      `${playerName} looks around only and to see jagged walls in close proximity. They stretch downward making the small passage even more narrow with every inch. The only possible way to fit is to crawl on the floor...\n>_`
+    );
   }
   //If player inputs 'examine', describe the room more
 
